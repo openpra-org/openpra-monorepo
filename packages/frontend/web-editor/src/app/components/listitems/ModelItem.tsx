445b1f5ee837880c6fb8befb400f91ca20879f92
sgsoleil <sgsoleil@ncsu.edu>
2023-06-14 16:53:23 -0400

    I updated the ui on the modelitems and models a bit

--- packages/frontend/web-editor/src/app/components/listitems/ModelItem.tsx
+++ packages/frontend/web-editor/src/app/components/listitems/ModelItem.tsx
@@ -1,4 +1,4 @@
-import { EuiCard, EuiIcon, EuiSpacer } from "@elastic/eui";
+import { EuiCard, EuiIcon, EuiSpacer, EuiFlexItem, EuiFlexGroup } from "@elastic/eui";
 
 export interface ModelItemProps {
   title: string,
@@ -13,13 +13,17 @@ export default function(props: ModelItemProps) {
   const { title, description } = props;
   return (
     <>
-    <EuiCard
-      layout="horizontal"
-      icon={<EuiIcon size="xl" type={'logoBeats'} />}
-      title={title}
-      description={description}
-      key={title}
-    />
+      {/* flex item is here to prevent the box from being too bit and limits its size */}
+        <EuiFlexItem grow={false} style={{ height: '100px' }}>
+          {/** cards are the easiest, each option should eb self explanitory, the icon is a placeholder for the Letter in a circle i */}
+          <EuiCard
+            layout="horizontal"
+            icon={<EuiIcon size="xl" type={'logoBeats'} />}
+            title={title}
+            description={description}
+            key={title}
+          />
+        </EuiFlexItem>
       <EuiSpacer size="m" />
     </>
   )
--- packages/frontend/web-editor/src/app/components/models.tsx
+++ packages/frontend/web-editor/src/app/components/models.tsx
@@ -1,8 +1,11 @@
 import ModelItem from "./listitems/ModelItem";
+import { EuiSpacer } from "@elastic/eui";
+
 
 export function Models(): JSX.Element {
     return (
       <>
+        <EuiSpacer size="s" />
         <ModelItem title="Title1" description="description" />
         <ModelItem title="Title1" description="description" />
         <ModelItem title="Title1" description="description" />

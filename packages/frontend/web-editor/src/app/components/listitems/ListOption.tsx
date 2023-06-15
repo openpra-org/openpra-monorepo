74074ba6335fc1915a78b9b40dd3ccf8fe875456
sgsoleil <sgsoleil@ncsu.edu>
2023-06-15 10:40:57 -0400

    Added the href to listOption and documented

--- packages/frontend/web-editor/src/app/components/listitems/ListOption.tsx
+++ packages/frontend/web-editor/src/app/components/listitems/ListOption.tsx
@@ -1,10 +1,14 @@
+//This is a ListOption used for when a drop down list occurs
+//Streamlined here and accounts for hrefs or on click actions
 import { EuiContextMenuItem } from "@elastic/eui";
 
+//List option takes in a key and a label garunteed
+//then takes an action to be done on click or an href to do on click, or nothing
 export interface ListOptionProps {
   key: string,
   label: string,
   action?: () => void;
-  href?: string;
+  hrefName?: string;
 }
 
 /**
@@ -12,10 +16,11 @@ export interface ListOptionProps {
  * @param props
  */
 export default function ListOption(props: ListOptionProps) {
-    const { key, label, action, href } = props;
-  
+    const { key, label, action, hrefName } = props;
+
+    //Returns a context menu item that does something, either an onclick function href depending on what is passed
     return (
-      <EuiContextMenuItem key={key} onClick={action}>
+      <EuiContextMenuItem key={key} onClick={action} href={hrefName}> 
         {label}
       </EuiContextMenuItem>
     );

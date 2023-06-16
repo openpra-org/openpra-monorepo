3895ac8661a1107f75272f86b997958360705bcc
sgsoleil <sgsoleil@ncsu.edu>
2023-06-16 15:45:02 -0400

    doc'ed up the new ModelItem

--- packages/frontend/web-editor/src/app/components/listitems/ModelItem.tsx
+++ packages/frontend/web-editor/src/app/components/listitems/ModelItem.tsx
@@ -35,17 +35,25 @@ export default function(props: ModelItemProps) {
           key={title}
           //On click function that will eventually navigate to the proper page
           //onClick={}
-        > {/* Card Content */}
+        > 
         </EuiCard>
       </EuiFlexItem>
-        {/** Rational for styling: These couldn't be in the card object themselves because they  */}
+        {/** Rational for styling: These couldn't be in the card object themselves because they cannot support than many compenents
+         * in addition the card really likes to align vertically instead of horizontally
+         * the negative top margin is to make it so there isnt a ton of space between the cards
+         * the position is to move it from underneath the card to on in in the middle on the right
+         * flex end puts it to the right of the page/flex group
+         */}
         <EuiFlexGroup justifyContent="flexEnd" style={{marginTop: "-25px", position: 'relative', top:'-35px', right:'20px'}}>
+            {/** This is where the text for last modified goes, eventually this should be updated accordingly */}
             <EuiFlexItem grow={false}>
               <EuiText>Last Modified:</EuiText>
             </EuiFlexItem>
+            {/** This is the settings/gear icon, moved 4 pixels as to line up a bit better */}
             <EuiFlexItem style={{ position: 'relative', top:'4px' }} grow={false} >
               <EuiIcon type={'gear'}/>
             </EuiFlexItem>
+            {/** This is the delete icon, moved 4 pixels as to line up a bit better, eventually this will lead to a pop up menu */}
             <EuiFlexItem style={{ position: 'relative', top:'4px' }} grow={false}>
               <EuiIcon type={'trash'}/>
             </EuiFlexItem>

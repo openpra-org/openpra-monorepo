4613967b25fb40c19da2bf328651f3d4989df334
sgsoleil <sgsoleil@ncsu.edu>
2023-06-19 15:32:46 -0400

    Added all the documentation for the new stuff!

--- packages/frontend/web-editor/src/app/components/newitem.tsx
+++ packages/frontend/web-editor/src/app/components/newitem.tsx
@@ -14,30 +14,38 @@ import {
     EuiSelectableOption
 } from '@elastic/eui'
 
+//list of props passed in, the users is optional and controls which version is shown, this is so we can reuse this structure later
   export interface NewItemProps {
     title: string
     users?: string[]
   }
   
 
-
+  //returns what is called a newItem, which is actually a panel to create a new item in some sort of list somewhere
 export default function NewItem(props: NewItemProps) {
 
+    //use the theme
     const {euiTheme} = useEuiTheme();
 
+    //grabbing the props
     const { title, users } = props;
 
+    //this is what is in the newItem strucutre, will eventually be used to actually make things
+    //this is also subject tyo change, propbably needs a type passed in from props eventually
     const newItem = {
         itemTitle: '',
         itemDescription: '',
         itemUsers: [] as string[]
     }
+
+    //use states that change things, called by functions later
     const [itemInfo, setItemInfo] =useState(newItem)
 
     const [options, setOptions] = useState<EuiSelectableOption[]>([]);
 
     const [selectedOptions, setSelectedOptions] = useState<EuiSelectableOption[]>([]);
 
+    //this shows which things have actually been selected
     useEffect(() => {
         if(users){
         const selectableOptions = users.map((user) => ({
@@ -47,6 +55,7 @@ export default function NewItem(props: NewItemProps) {
         }
     }, [users]);
 
+    //this does something if users exist, it will map the users in the new item to the selected users
     const handleOptionChange = (newOptions: EuiSelectableOption[]) => {
         const selectedUsers = newOptions.map((option) => option.label);
         setItemInfo({
@@ -56,8 +65,10 @@ export default function NewItem(props: NewItemProps) {
       };
 
     return (
+            //this styling is so its in a nice looking box, it scales if the users tab is there or not
             <EuiForm style={{backgroundColor: euiTheme.colors.lightShade, alignSelf: 'center', width: '500px', borderRadius: '5px'}}>
                 <EuiSpacer size='s'/>
+                {/** this gives the text, and then importantly sets the title of the item */}
                 <EuiTextColor style={{margin: '10px', fontSize: '30px'}}><strong>{title}</strong></EuiTextColor>
                 <EuiFormRow fullWidth={true} style={{margin: '10px'}}>
                     <EuiFieldText
@@ -70,6 +81,7 @@ export default function NewItem(props: NewItemProps) {
                         })}
                     />
                 </EuiFormRow>
+                {/** this form row is for the description */}
                 <EuiFormRow fullWidth={true} style={{margin: '10px'}}>
                     <EuiTextArea
                         fullWidth={true}
@@ -82,6 +94,7 @@ export default function NewItem(props: NewItemProps) {
                         })}
                     />
                 </EuiFormRow>
+                {/** toggles if users exists and is passed, and it shows the selectable menu of users */}
                 {users &&(
                     <>
                         <EuiFormRow fullWidth={true} style={{margin: '10px'}}>
@@ -103,6 +116,7 @@ export default function NewItem(props: NewItemProps) {
                         </EuiFormRow>
                     </>
                 )}
+                {/** the submit and also the go back buttons are right here*/}
                 <EuiFormRow fullWidth={true}>
                     <EuiFlexGroup justifyContent='spaceBetween' gutterSize='xs' style={{margin: '5px'}}>
                         <EuiFlexItem>

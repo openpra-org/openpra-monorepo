bd9e524c24d7b7c57a1552b98f34080ece8ac10e
sgsoleil <sgsoleil@ncsu.edu>
2023-06-21 16:05:25 -0400

    fixed minor formatting issues with datasidenav, and made pages link, fixed data pages temp format as well

--- packages/frontend/web-editor/src/app/components/smallcomponents/dataSidenav.tsx
+++ packages/frontend/web-editor/src/app/components/smallcomponents/dataSidenav.tsx
@@ -1,18 +1,18 @@
 import {
-    EuiCollapsibleNav,
     EuiCollapsibleNavGroup,
-    EuiFlexGroup,
     EuiIcon,
     EuiListGroup,
-    EuiSideNav,
-    htmlIdGenerator,
     useEuiTheme
 } from "@elastic/eui";
-import {useState} from 'react'
 
 export default function DataSidenav() {
     const {euiTheme} = useEuiTheme();
 
+    //This one is much more simple and parsable than modelSidenav, 
+    //The documentation for how this structure works will be in modelSideNav, if this ends up being complicated I will copy paste it
+    //They function the same and look the same, just have different datasets mostly, and there is an additional change to modelSidenav in the implementation
+    //of mapping through items. 
+    //we didnt use an item to import because elastic ui doesnt love when a very complicated structure such as this is passed.
     const navItems =
         {
             id: 'mainNavGroup',
@@ -23,29 +23,34 @@ export default function DataSidenav() {
                     title: 'Parameter Estimates',
                     items: [
                         {
-                            id: 'analysisNavGroup',
-                            label: 'Open State Analysis',
-                            icon: <EuiIcon type='branch'/>
-                        },
-                        {
                             id: 'specialEventsNavGroup',
                             label: 'Special Events',
-                            icon: <EuiIcon type='branch'/>
+                            icon: <EuiIcon type='branch'/>,
+                            href: 'data/specialevents'
                         },
                         {
                             id: 'componentReliabilityNavGroup',
                             label: 'Component Reliability',
-                            icon: <EuiIcon type='branch'/>
+                            icon: <EuiIcon type='branch'/>,
+                            href: 'data/componentreliability'
                         },
                         {
                             id: 'initiatingEventsNavGroup',
                             label: 'Initiating Events',
-                            icon: <EuiIcon type='branch'/>
+                            icon: <EuiIcon type='branch'/>,
+                            href: 'data/initiatingevents'
+                        },
+                        {
+                            id: 'trainUA',
+                            label: 'Train UA',
+                            icon: <EuiIcon type='branch'/>,
+                            href: 'data/trainua'
                         },
                         {
                             id: 'ccfNavGroup',
                             label: 'CCF',
-                            icon: <EuiIcon type='branch'/>
+                            icon: <EuiIcon type='branch'/>,
+                            href: 'data/ccf'
                         },
                     ]
                 }
@@ -55,10 +60,11 @@ export default function DataSidenav() {
         }
         // Add more groups and items as needed
     return (
+        //iterates through the parent, then the children, then the grandchildren where the clickables with icons and hrefs are.
         <EuiCollapsibleNavGroup
             key={navItems.id}
             title={navItems.title}
-            style={{maxWidth: '250px', backgroundColor: euiTheme.colors.mediumShade}}
+            style={{maxWidth: '250px', backgroundColor: euiTheme.colors.lightShade}}
             isCollapsible={true}
             initialIsOpen={true}
         >

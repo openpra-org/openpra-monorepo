a4585dde4cd121995449bdee9df9aad100e0e910
sgsoleil <sgsoleil@ncsu.edu>
2023-06-23 12:54:10 -0400

    Made a general page layout so we dont have to do everything like 10 times

--- packages/frontend/web-editor/src/app/components/largecomponents/pageLayout.tsx
+++ packages/frontend/web-editor/src/app/components/largecomponents/pageLayout.tsx
@@ -1,17 +1,33 @@
 //this is all placeholder so that I can test hrefs and stuff
 
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 import { ModelSubHeader } from '../../components/largecomponents/modelSubHeader'
 import {PageHeader, ModelPageFilter} from '../../components/smallcomponents/headers'
 import {EuiFlexGroup, EuiFlexItem} from '@elastic/eui'
 
-interface ModelSidenavProps {
-    isNavOpen: boolean;
-    onNavToggle: (isOpen: boolean) => void;
-  }  
+interface PageLayoutProps {
+    pageName: string;
+    contentType: React.ReactNode;
+}
 
-export default function EventSequenceDiagrams() {
+export default function PageLayout({pageName, contentType}: PageLayoutProps) {
     const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const [pageWidth, setPageWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        // Update the window size whenever the window is resized
+        const handleResize = () => {
+            setPageWidth(window.innerWidth)
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Clean up the event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     const handleNavToggle = (isOpen: boolean) => {
         setIsNavOpen(isOpen);
@@ -22,11 +38,16 @@ export default function EventSequenceDiagrams() {
             <PageHeader />
 
             <EuiFlexGroup direction='column'>
-                <EuiFlexItem>
-                    <ModelSubHeader isNavOpen={isNavOpen} onNavToggle={handleNavToggle} pageName='Event Sequence Diagrams'/>
+                <EuiFlexItem grow={false}>
+                    <ModelSubHeader isNavOpen={isNavOpen} onNavToggle={handleNavToggle} pageName={pageName}/>
                 </EuiFlexItem>
-                <EuiFlexItem grow={10} style={{ width: isNavOpen ? '200px' : '1000px' }} >
+                {/** This is here to make its width toggle based on whether the menu is down or not, the 335 roughly deals with the amount of space with the menu, and because
+                 * this is uniform it should stay steady throughout. This section is where page specific content should go
+                 * 
+                 */}
+                <EuiFlexItem grow={10} style={{ marginLeft: "auto", width: isNavOpen ? window.innerWidth : window.innerWidth - 335 }} >
                     <ModelPageFilter/>
+                    {contentType}
                 </EuiFlexItem>
             </EuiFlexGroup>
         </>
--- packages/frontend/web-editor/src/app/pages/modelPages/basicEvents.tsx
+++ packages/frontend/web-editor/src/app/pages/modelPages/basicEvents.tsx
@@ -1,56 +1,10 @@
-//this is all placeholder so that I can test hrefs and stuff
+import PageLayout from '../../components/largecomponents/pageLayout';
 
-import {useEffect, useState} from 'react';
-import { ModelSubHeader } from '../../components/largecomponents/modelSubHeader'
-import {PageHeader, ModelPageFilter} from '../../components/smallcomponents/headers'
-import {EuiFlexGroup, EuiFlexItem} from '@elastic/eui'
-
-interface ModelSidenavProps {
-    isNavOpen: boolean;
-    onNavToggle: (isOpen: boolean) => void;
-  }
-
-
-
+//documenting how this works here, basically it just uses pagelayout and passes it content, so go there for more!
 export default function BasicEvents() {
-    const [isNavOpen, setIsNavOpen] = useState(false);
-
-    const [pageWidth, setPageWidth] = useState(window.innerWidth);
-
-    useEffect(() => {
-        // Update the window size whenever the window is resized
-        const handleResize = () => {
-            setPageWidth(window.innerWidth)
-        };
-
-        window.addEventListener('resize', handleResize);
-
-        // Clean up the event listener on component unmount
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    const handleNavToggle = (isOpen: boolean) => {
-        setIsNavOpen(isOpen);
-      };
-
     return (
         <> 
-            <PageHeader />
-
-            <EuiFlexGroup direction='column'>
-                <EuiFlexItem>
-                    <ModelSubHeader isNavOpen={isNavOpen} onNavToggle={handleNavToggle} pageName='Basic Events'/>
-                </EuiFlexItem>
-                {/** This is here to make its width toggle based on whether the menu is down or not, the 335 roughly deals with the amount of space with the menu, and because
-                 * this is uniform it should stay steady throughout. This section is where page specific content should go
-                 * 
-                 */}
-                <EuiFlexItem grow={10} style={{ marginLeft: "auto", width: isNavOpen ? window.innerWidth : window.innerWidth - 335 }} >
-                    <ModelPageFilter/>
-                </EuiFlexItem>
-            </EuiFlexGroup>
+            <PageLayout pageName='Basic Events' contentType={<div/>}/>
         </>
     )
 }

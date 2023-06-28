9f46f17c8880fb13b2fac4745ff4abb36c5a0f87
sgsoleil <sgsoleil@ncsu.edu>
2023-06-28 16:23:56 -0400

    Work on dummying out the model settings page a bit

---
+++ packages/frontend/web-editor/src/app/components/largecomponents/modelOptionsMenu.tsx
@@ -0,0 +1,56 @@
+import { EuiAccordion, EuiFlexGroup, EuiFlexItem, EuiText } from "@elastic/eui";
+import ModelItemsList from "./modelItemsList";
+
+
+export default function(){
+
+    const overviewLabel = (
+        <EuiFlexGroup gutterSize="s" alignItems="center" responsive={false}>
+        </EuiFlexGroup>
+    )
+
+    return(
+        < div style={{marginLeft: "10px", marginRight: "10px", marginTop: "-20px"}}>
+            <EuiAccordion
+            id="accordion11"
+            arrowDisplay="right"
+            buttonContent="Overview"
+            paddingSize="s">
+                <ModelItemsList/>
+            </EuiAccordion>
+            <EuiAccordion
+            id="accordion11"
+            arrowDisplay="right"
+            buttonContent="General"
+            paddingSize="s">
+                <EuiText>
+                    <p>
+                    Any content inside of <strong>EuiAccordion</strong> will appear here.
+                    </p>
+                </EuiText>
+            </EuiAccordion>
+            <EuiAccordion
+            id="accordion11"
+            arrowDisplay="right"
+            buttonContent="Permissions"
+            paddingSize="s">
+                <EuiText>
+                    <p>
+                    Any content inside of <strong>EuiAccordion</strong> will appear here.
+                    </p>
+                </EuiText>
+            </EuiAccordion>
+            <EuiAccordion
+            id="accordion11"
+            arrowDisplay="right"
+            buttonContent="Advanced"
+            paddingSize="s">
+                <EuiText>
+                    <p>
+                    Any content inside of <strong>EuiAccordion</strong> will appear here.
+                    </p>
+                </EuiText>
+            </EuiAccordion>
+        </div>
+    )
+}
--- packages/frontend/web-editor/src/app/pages/modelPages/modelSettings.tsx
+++ packages/frontend/web-editor/src/app/pages/modelPages/modelSettings.tsx
@@ -1,9 +1,14 @@
+import ModelOptionsMenu from '../../components/largecomponents/modelOptionsMenu';
 import PageLayout from '../../components/largecomponents/pageLayout';
 
 export default function ModelSettings() {
     return (
         <> 
-            <PageLayout isModel={true} pageName='Model Settings' contentType={<div/>}/>
+            <PageLayout isModel={true} pageName='Model Settings' contentType={
+                <>
+                <ModelOptionsMenu/>
+                </>
+            }/>
         </>
     )
 }

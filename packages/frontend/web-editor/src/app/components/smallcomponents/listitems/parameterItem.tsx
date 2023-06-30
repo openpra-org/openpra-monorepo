c3fde701e324743f3b580f3a513a2e779c735b17
Pixel143 <91994470+Pixel143@users.noreply.github.com>
2023-06-30 11:00:08 -0400

    Added parameter list. Still needs some styling

--- packages/frontend/web-editor/src/app/components/smallcomponents/listitems/parameterItem.tsx
+++ packages/frontend/web-editor/src/app/components/smallcomponents/listitems/parameterItem.tsx
@@ -1,18 +1,32 @@
-import { EuiFlexGroup, EuiFlexItem, EuiText, useEuiTheme } from "@elastic/eui";
+import {EuiFlexGroup, EuiFlexItem, EuiText, EuiIcon, useEuiTheme} from "@elastic/eui";
 
 
 export default function (){
 
+    const parameters = [
+        {name: 'Parameter1', value: '5000'},
+        {name: 'Parameter2', value: '6000'}
+    ]
+
     const {euiTheme} = useEuiTheme();
 
     return (
-        <EuiFlexGroup style={{backgroundColor: euiTheme.colors.lightShade, width: "500px"}}>
-            <EuiFlexItem>
-                <EuiText> Parameter </EuiText>
+        <EuiFlexGroup direction='column'>
+            <EuiFlexItem grow={false} style={{margin: '10px', padding: '10px'}}>
+                <EuiFlexGroup>
+                    <EuiText style={{fontSize: '2rem', width: '19%'}}>Parameter</EuiText>
+                    <EuiText style={{fontSize: '2rem'}}>Value</EuiText>
+                </EuiFlexGroup>
             </EuiFlexItem>
-            <EuiFlexItem>
-                <EuiText> 5000 </EuiText>
-            </EuiFlexItem>
+            {parameters.map((param)=>(
+                    <EuiFlexItem grow={false} style={{backgroundColor: euiTheme.colors.lightShade, margin: '10px', padding: '10px'}}>
+                        <EuiFlexGroup direction='row'>
+                            <EuiText style={{width: '20%'}}> {param.name}</EuiText>
+                            <EuiText style={{width: '70%'}}> {param.value} </EuiText>
+                            <EuiIcon style={{width: '10%'}} type='trash'/>
+                        </EuiFlexGroup>
+                    </EuiFlexItem>
+            ))}
         </EuiFlexGroup>
     )
 }
--- packages/frontend/web-editor/src/app/pages/modelPages/modelGlobalParameters.tsx
+++ packages/frontend/web-editor/src/app/pages/modelPages/modelGlobalParameters.tsx
@@ -1,11 +1,13 @@
 import GlobalParametersList from '../../components/largecomponents/lists/globalParametersList';
 import PageLayout from '../../components/largecomponents/stylingaids/pageLayout';
+import {ModelPageFilter} from "../../components/largecomponents/headers/headers";
 
 export default function BasicEvents() {
     return (
         <> 
             <PageLayout isModel={true} pageName='Basic Events' contentType={
                 <>
+                    <ModelPageFilter title="Global Parameters" page="model/1/globalParameters" />
                     <GlobalParametersList/>
                 </>
             }/>

e228ce4b25690d4e4282657e3a29ec9b291c96e7
sgsoleil <sgsoleil@ncsu.edu>
2023-06-30 15:55:12 -0400

    added some documentation and renamed some things, maintence

--- packages/frontend/web-editor/src/app/components/largecomponents/lists/globalParametersList.tsx
+++ packages/frontend/web-editor/src/app/components/largecomponents/lists/globalParametersList.tsx
@@ -1,7 +1,7 @@
 import { EuiFlexGroup, EuiFlexItem, EuiText, useEuiTheme } from "@elastic/eui";
-import ParameterItem from "../../smallcomponents/listitems/parameterItem";
+import ParameterItem from "../../smallcomponents/listitems/parameterItems";
 
-
+//as of right now this just returns the list of parameter items in parameter items
 export default function (){
 
     const {euiTheme} = useEuiTheme();

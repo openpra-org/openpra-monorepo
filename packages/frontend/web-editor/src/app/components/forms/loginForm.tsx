801dc6a75534e6377bddf72aca45ef3588a560de
rasheeqqua <rasheeq1018182@gmail.com>
2023-07-03 20:46:24 +0600

    Modularized SignupForm and LoginForm component

---
+++ packages/frontend/web-editor/src/app/components/forms/loginForm.tsx
@@ -0,0 +1,117 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import {
+    EuiFieldText,
+    EuiForm,
+    EuiButton,
+    EuiFormRow,
+    EuiFieldPassword
+} from "@elastic/eui";
+import { LoginProps, LoginErrorProps } from "../../../../../../shared-types/src/lib/api/AuthTypes";
+import ApiManager from "../../../../../../shared-types/src/lib/api/ApiManager";
+
+export default function LoginForm() {
+    console.log("LoginForm(), 1");
+    const DefaultProps: LoginProps = {
+        username: '',
+        password: '',
+    }
+    const DefaultErrorProps: LoginErrorProps = {
+        username: false,
+        password: false,
+    }
+
+    const [login, setLogin] = useState(DefaultProps);
+    const [error, setError] = useState(DefaultErrorProps);
+    const [ redirectToHomepage, setRedirectToHomepage ] = useState(false);
+
+    function handleLogin() {
+        console.log("handleLogin(), 9");
+        const { username, password } = login;
+        ApiManager.signInWithUsernameAndPassword(username, password)
+            .then(() => {
+                if(ApiManager.isLoggedIn()) {
+                    setRedirectToHomepage(true)
+                }
+            })
+    }
+
+    //Corrects the isInvalid when a user types something in a blank input field
+    useEffect(() => {
+        if(login.username && error.username) {
+            setError({
+                ...error,
+                username: false
+            })
+        }
+        if(login.password && error.password) {
+            setError({
+                ...error,
+                password: false
+            })
+        }
+    }, [login])
+
+    function validateLogin(e: React.FormEvent<HTMLFormElement>): void {
+        console.log("validateLogin(), 3");
+        e.preventDefault()
+
+        //need errorCheck in the later if statement due to how states and renders work
+        let errorCheck = error;
+        errorCheck = {
+            username: (!login.username),
+            password: (!login.password)
+        }
+
+        setError({
+            username: (!login.username),
+            password: (!login.password)
+        })
+
+        //makes sure all input fields are not empty
+        if(!errorCheck.username && !errorCheck.password) {
+            handleLogin()
+        }
+    }
+
+    if(redirectToHomepage) {
+        return (
+            <Navigate to="models" replace={true} />
+        );
+    } else {
+        return (
+            <EuiForm component="form" onSubmit={validateLogin}>
+                <br/>
+                <EuiFormRow>
+                    <EuiFieldText
+                        placeholder="Username"
+                        isInvalid={error.username}
+                        value={login.username}
+                        onChange={(e) => setLogin({
+                            ...login,
+                            username: e.target.value
+                        })}
+                    />
+                </EuiFormRow>
+                <EuiFormRow>
+                    <EuiFieldPassword
+                        type="dual"
+                        placeholder="Password"
+                        isInvalid={error.password}
+                        value={login.password}
+                        onChange={(e) => setLogin({
+                            ...login,
+                            password: e.target.value
+                        })}
+                    />
+                </EuiFormRow>
+                <EuiFormRow>
+                    <EuiButton type="submit">
+                        Login
+                    </EuiButton>
+                </EuiFormRow>
+            </EuiForm>
+        );
+    }
+}
---
+++ packages/frontend/web-editor/src/app/components/forms/signupForm.tsx
@@ -0,0 +1,213 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import {
+    EuiFieldText,
+    EuiForm,
+    EuiButton,
+    EuiFormRow,
+    EuiFieldPassword
+} from "@elastic/eui";
+import { SignUpErrorProps, SignUpProps } from "../../../../../../shared-types/src/lib/api/AuthTypes";
+import ApiManager from "../../../../../../shared-types/src/lib/api/ApiManager";
+
+export default function SignupForm() {
+    console.log("signupForm(), 4");
+    const defaultProps: SignUpProps = {
+        username: '',
+        email: '',
+        firstName: '',
+        lastName: '',
+        password: '',
+        passConfirm: '',
+    }
+    const defaultErrorProps: SignUpErrorProps = {
+        username: false,
+        email: false,
+        firstName: false,
+        lastName: false,
+        password: false,
+        passConfirm: false,
+    }
+    const [signup, setSignup] = useState(defaultProps)
+    const [error, setError] = useState(defaultErrorProps)
+    const [redirectToHomepage, setRedirectToHomepage] = useState(false)
+
+    let passError = '';
+
+
+    function handleSignup() {
+        console.log("handleSignup(), 5");
+        const { passConfirm, ...signupData } = signup;
+        ApiManager.signup(signupData)
+          .then(() => {
+            if(ApiManager.isLoggedIn()) {
+                setRedirectToHomepage(true)
+            }
+          })
+    }
+
+    //Corrects the isInvalid when a user types something in a blank input field
+    useEffect(() => {
+        if(signup.firstName && error.firstName) {
+            setError({
+                ...error,
+                firstName: false
+            })
+        }
+        if(signup.lastName && error.lastName) {
+            setError({
+                ...error,
+                lastName: false
+            })
+        }
+        if(signup.email && error.email) {
+            setError({
+                ...error,
+                email: false
+            })
+        }
+        if(signup.username && error.username) {
+            setError({
+                ...error,
+                username: false
+            })
+        }
+        if(signup.password && error.password) {
+            setError({
+                ...error,
+                password: false
+            })
+        }
+        if(signup.passConfirm && error.passConfirm) {
+            setError({
+                ...error,
+                passConfirm: false
+            })
+        }
+    }, [signup])
+
+    function validateSignup(e: React.FormEvent<HTMLFormElement>): void {
+        console.log("validateSignup, 6");
+        e.preventDefault()
+
+        const confirmPasswords = (signup.password === signup.passConfirm);
+        console.log(confirmPasswords);
+
+        //need errorCheck in the later if statement due to how states and renders work
+        let errorCheck = error;
+        errorCheck = {
+            firstName: (!signup.firstName),
+            lastName: (!signup.lastName),
+            email: (!signup.email),
+            username: (!signup.username),
+            password: (!signup.password),
+            passConfirm: (!signup.passConfirm || !confirmPasswords)
+        }
+
+        setError({
+            firstName: (!signup.firstName),
+            lastName: (!signup.lastName),
+            email: (!signup.email),
+            username: (!signup.username),
+            password: (!signup.password),
+            passConfirm: (!signup.passConfirm || !confirmPasswords)
+        })
+
+        passError = (!confirmPasswords ? 'Passwords do not match' : '');
+        console.log(passError);
+
+        //makes sure all input fields are not empty and that both passwords match
+        if(
+            !errorCheck.firstName && !errorCheck.lastName && !errorCheck.email && !errorCheck.username &&
+            !errorCheck.password && !errorCheck.passConfirm && confirmPasswords
+        ) {
+            handleSignup()
+        }
+    }
+
+    if(redirectToHomepage) {
+        return (
+            <Navigate to="models" replace={true} />
+        );
+    } else {
+        return (
+            <EuiForm component="form" onSubmit={validateSignup} error={passError}>
+                <br/>
+                <EuiFormRow>
+                    <EuiFieldText
+                        placeholder="First name"
+                        isInvalid={error.firstName}
+                        value={signup.firstName}
+                        onChange={(e) => setSignup({
+                            ...signup,
+                            firstName: e.target.value
+                        })}
+                    />
+                </EuiFormRow>
+                <EuiFormRow>
+                    <EuiFieldText
+                        placeholder="Last name"
+                        isInvalid={error.lastName}
+                        value={signup.lastName}
+                        onChange={(e) => setSignup({
+                            ...signup,
+                            lastName: e.target.value
+                        })}
+                    />
+                </EuiFormRow>
+                <EuiFormRow>
+                    <EuiFieldText
+                        placeholder="Email"
+                        isInvalid={error.email}
+                        value={signup.email}
+                        onChange={(e) => setSignup({
+                            ...signup,
+                            email: e.target.value
+                        })}
+                    />
+                </EuiFormRow>
+                <EuiFormRow>
+                    <EuiFieldText
+                        placeholder="Username"
+                        isInvalid={error.username}
+                        value={signup.username}
+                        onChange={(e) => setSignup({
+                            ...signup,
+                            username: e.target.value
+                        })}
+                    />
+                </EuiFormRow>
+                <EuiFormRow>
+                    <EuiFieldPassword
+                        type="dual"
+                        placeholder="Password"
+                        isInvalid={error.password}
+                        value={signup.password}
+                        onChange={(e) => setSignup({
+                            ...signup,
+                            password: e.target.value
+                        })}
+                    />
+                </EuiFormRow>
+                <EuiFormRow isInvalid={error.passConfirm} error={passError}>
+                    <EuiFieldPassword
+                        type="dual"
+                        placeholder="Confirm Password"
+                        isInvalid={error.passConfirm}
+                        value={signup.passConfirm}
+                        onChange={(e) => setSignup({
+                            ...signup,
+                            passConfirm: e.target.value
+                        })}
+                    />
+                </EuiFormRow>
+                <EuiFormRow>
+                    <EuiButton type="submit">
+                        Sign Up
+                    </EuiButton>
+                </EuiFormRow>
+            </EuiForm>
+        );
+    }
+}

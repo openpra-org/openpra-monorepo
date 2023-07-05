535e04591e9491bc7e1b03b38f4119b493ee756b
sgsoleil <sgsoleil@ncsu.edu>
2023-07-05 14:28:33 -0400

    Updated dataTable to work correctly as a data grid

--- packages/frontend/web-editor/src/app/components/largecomponents/tables/tabletemplate/dataTable.tsx
+++ packages/frontend/web-editor/src/app/components/largecomponents/tables/tabletemplate/dataTable.tsx
@@ -1,43 +1,46 @@
 import { EuiDataGrid, EuiFlexGroup, EuiFlexItem, EuiThemeProvider, useEuiTheme } from '@elastic/eui';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+//
 interface DataTableProps {
   rows: any[];
   columns: any[];
 }
 
+//the indexes in a sell
 interface CellValueProps {
   rowIndex: number;
   colIndex: number;
 }
 
 export default function DataTable({ rows, columns }: DataTableProps) {
+
+  //declares the theme if we need to style it
   const { euiTheme } = useEuiTheme();
 
+  //this renders the content, it uses visible columns instead of just columns so everything shows up as intended, with the correct id
+  //EUI hands the complexities.
   const renderCellValue = ({ rowIndex, colIndex }: CellValueProps) => {
-    const columnId = columns[colIndex].id;
+    const visibleColumnId = visibleColumns[colIndex];
     const row = rows[rowIndex];
-    const value = row[columnId];
+    const value = row[visibleColumnId];
     return value;
   };
 
-  const [visibleColumns, setVisibleColumns] = useState(() => [...columns]);
-
-  const onColumnSort = (columns: any[]) => {
-    setVisibleColumns(columns.map((column) => column.id));
-  };
+  //mapping the initial data here to the visible columns
+  const [visibleColumns, setVisibleColumns] = useState(columns.map((column) => column.id));
 
   return (
-      <EuiFlexGroup style={{ margin: '9px' }} className="eui-xScroll">
-        <EuiFlexItem grow={true}>
-          <EuiDataGrid
-            columns={columns}
-            columnVisibility={{ visibleColumns, setVisibleColumns }}
-            rowCount={rows.length}
-            renderCellValue={renderCellValue}
-            aria-labelledby="dataGridLabel"
-          />
-        </EuiFlexItem>
-      </EuiFlexGroup>
+    <EuiFlexGroup style={{ margin: '9px' }} className="eui-xScroll">
+      <EuiFlexItem grow={true}>
+        <EuiDataGrid
+          columns={columns}
+          columnVisibility={{ visibleColumns, setVisibleColumns }}
+          rowCount={rows.length}
+          renderCellValue={renderCellValue}
+          aria-labelledby="dataGridLabel"
+        />
+      </EuiFlexItem>
+    </EuiFlexGroup>
   );
 }

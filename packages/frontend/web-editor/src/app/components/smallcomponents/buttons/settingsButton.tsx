d01c0b71513de3089f129806b893f24cfaccd511
sgsoleil <sgsoleil@ncsu.edu>
2023-07-05 08:47:12 -0400

    Documentated

--- packages/frontend/web-editor/src/app/components/smallcomponents/buttons/settingsButton.tsx
+++ packages/frontend/web-editor/src/app/components/smallcomponents/buttons/settingsButton.tsx
@@ -1,5 +1,7 @@
 import {useEuiTheme, EuiButton, EuiButtonProps} from '@elastic/eui'
 
+//takes in an onclick because it didnt like me sennding it innately
+//and also style overrides so that we can set any style we want to
 interface SettingsButtonProps extends EuiButtonProps {
     onClick?: React.MouseEventHandler
     styleOverrides?: React.CSSProperties
@@ -7,10 +9,13 @@ interface SettingsButtonProps extends EuiButtonProps {
 
 export default function SettingsButton(props: SettingsButtonProps) {
 
+    //list of props passed
     const { onClick, styleOverrides, ...buttonProps} = props;
 
+    //uses theme to grab the color we want
     const {euiTheme} = useEuiTheme();
 
+    //sets the settings button style to what we want when we reuse it, this could even be used as a standard button to be honest
     const settingsButtonStyle = {
         borderRadius: '5px', 
         backgroundColor: euiTheme.colors.mediumShade, 
@@ -18,6 +23,7 @@ export default function SettingsButton(props: SettingsButtonProps) {
         ...styleOverrides
     }
 
+    //just returns an eui button with our styling
     return(
         <EuiButton {...buttonProps} color='text' onClick={onClick} style={settingsButtonStyle} />
     )

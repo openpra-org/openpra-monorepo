440eac0968854ceea7d7a4efbb17f314633c0ae4
Arjun Earthperson <aarjun@ncsu.edu>
2023-07-07 11:47:58 -0400

    Fixed styling in landing page

--- packages/frontend/web-editor/src/app/pages/landingPage.tsx
+++ packages/frontend/web-editor/src/app/pages/landingPage.tsx
@@ -1,23 +1,54 @@
-import { EuiPage, EuiText, EuiPageBody, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { EuiPage, EuiText, EuiHideFor, EuiPageBody, EuiFlexGroup, EuiFlexItem, useEuiPaddingCSS} from '@elastic/eui';
 import AuthCard from "../components/cards/authCard";
-
 export default function LandingPage() {
+    const paddingStyles = useEuiPaddingCSS();
+    const cssStyles = [paddingStyles["xl"]];
     return (
         <EuiPage restrictWidth>
             <EuiPageBody>
-                <EuiFlexGroup justifyContent="spaceAround" alignItems="center">
-                    <EuiFlexItem grow={7}>
-                        <EuiText grow={true}>
-                            <h1>This is Heading One</h1>
-                            <p>
-                                Far out in the uncharted backwaters of the unfashionable {' '}
-                                end of the western spiral arm of the Galaxy lies a small unregarded
-                                yellow sun. When suddenly some wild JavaScript code appeared!{' '}
-                                <code>const whoa = &quot;!&quot;</code>
-                            </p>
-                        </EuiText>
-                    </EuiFlexItem>
-                    <EuiFlexItem grow={3} style={{ alignItems: "center"}}>
+                <EuiFlexGroup justifyContent="spaceAround" alignItems="center" gutterSize="none">
+                    <EuiHideFor sizes={['xs', 's']}>
+                        <EuiFlexItem grow={7} css={cssStyles}>
+                            <EuiText grow={true}>
+                                <h1>Welcome to OpenPRA</h1>
+                                <p>
+                                    Far out in the uncharted backwaters of the unfashionable {' '}
+                                    end of the western spiral arm of the Galaxy lies a small unregarded
+                                    yellow sun. When suddenly some wild JavaScript code appeared!{' '}
+                                    <code>const whoa = &quot;!&quot;</code>
+                                    Far out in the uncharted backwaters of the unfashionable {' '}
+                                    end of the western spiral arm of the Galaxy lies a small unregarded
+                                    yellow sun. When suddenly some wild JavaScript code appeared!{' '}
+                                    <code>const whoa = &quot;!&quot;</code>
+                                    Far out in the uncharted backwaters of the unfashionable {' '}
+                                    end of the western spiral arm of the Galaxy lies a small unregarded
+                                    yellow sun. When suddenly some wild JavaScript code appeared!{' '}
+                                    <code>const whoa = &quot;!&quot;</code>
+                                </p>
+                                <p>
+                                    Far out in the uncharted backwaters of the unfashionable {' '}
+                                    end of the western spiral arm of the Galaxy lies a small unregarded
+                                    yellow sun. When suddenly some wild JavaScript code appeared!{' '}
+                                    <code>const whoa = &quot;!&quot;</code>
+                                </p>
+                                <p>
+                                    Far out in the uncharted backwaters of the unfashionable {' '}
+                                    end of the western spiral arm of the Galaxy lies a small unregarded
+                                    yellow sun. When suddenly some wild JavaScript code appeared!{' '}
+                                    <code>const whoa = &quot;!&quot;</code>
+                                    Far out in the uncharted backwaters of the unfashionable {' '}
+                                    end of the western spiral arm of the Galaxy lies a small unregarded
+                                    yellow sun. When suddenly some wild JavaScript code appeared!{' '}
+                                    <code>const whoa = &quot;!&quot;</code>
+                                    Far out in the uncharted backwaters of the unfashionable {' '}
+                                    end of the western spiral arm of the Galaxy lies a small unregarded
+                                    yellow sun. When suddenly some wild JavaScript code appeared!{' '}
+                                    <code>const whoa = &quot;!&quot;</code>
+                                </p>
+                            </EuiText>
+                        </EuiFlexItem>
+                    </EuiHideFor>
+                    <EuiFlexItem grow={false}>
                         <AuthCard />
                     </EuiFlexItem>
                 </EuiFlexGroup>

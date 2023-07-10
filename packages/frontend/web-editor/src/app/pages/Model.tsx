1e54cbe77991728673017308829672bafddf087e
Arjun Earthperson <aarjun@ncsu.edu>
2023-07-10 16:14:18 -0400

    added Outlet in Model page with sidebar

--- packages/frontend/web-editor/src/app/pages/Model.tsx
+++ packages/frontend/web-editor/src/app/pages/Model.tsx
@@ -1,8 +1,31 @@
-import { useLoaderData, useRouteLoaderData } from "react-router-dom";
+import { Outlet, useLoaderData, useRouteLoaderData } from "react-router-dom";
 import { ModelProps } from "./ModelsPage";
 
-export default function Model() {
-  const model = useLoaderData();
-  console.log(model);
-  return <p>I'm a model page with modelId `${model}`</p>
-}
+import React, { ReactElement } from 'react';
+import {
+  EuiText,
+  EuiPageTemplate,
+  EuiPageTemplateProps,
+  EuiPageHeaderProps,
+  EuiPageSidebarProps,
+} from '@elastic/eui';
+
+export default () => {
+  return (
+    <EuiPageTemplate
+    >
+      <EuiPageTemplate.Sidebar>
+        sidebar stuff
+      </EuiPageTemplate.Sidebar>
+      <EuiPageTemplate.Section grow={false} bottomBorder>
+        <EuiText textAlign="center">
+          <strong>
+            Stack EuiPageTemplate sections and headers to create your custom
+            content order.
+          </strong>
+        </EuiText>
+        <Outlet />
+      </EuiPageTemplate.Section>
+    </EuiPageTemplate>
+  );
+};

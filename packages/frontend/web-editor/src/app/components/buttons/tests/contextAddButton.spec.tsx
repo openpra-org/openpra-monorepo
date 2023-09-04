2a4ae1b5edb57d8e36a2f725ed31fc564327923c
sgsoleil <sgsoleil@ncsu.edu>
2023-09-04 10:20:48 -0400

    Finally found a way to load things that use location if they need to

--- packages/frontend/web-editor/src/app/components/buttons/tests/contextAddButton.spec.tsx
+++ packages/frontend/web-editor/src/app/components/buttons/tests/contextAddButton.spec.tsx
@@ -1,8 +1,15 @@
 import { render } from '@testing-library/react';
 import ContextAddButton from '../contextAddButton';
 
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useLocation: () => ({
+      pathname: "localhost:4200/internal-events"
+    })
+  }));
+
 describe('ContextAddButton', () => {
     it('renders button with popover', () => {
         const { getByText, getByAltText, getByTestId} = render(<ContextAddButton />);
     });
-  });
+});

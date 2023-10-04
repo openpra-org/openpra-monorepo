776bb5199844261c97205c1df808e5704f361975
Arjun Earthperson <aarjun@ncsu.edu>
2023-10-04 15:02:00 -0400

    update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,3 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-  e2e: {
-    baseUrl:'http://localhost:8000',
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({

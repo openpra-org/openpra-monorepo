1dc1a0da1d904d00eb2a7969c81ce0d3603e977f
Arjun Earthperson <aarjun@ncsu.edu>
2023-11-16 16:00:08 -0500

    update main.tsx with comments

--- packages/frontend/web-editor/src/main.tsx
+++ packages/frontend/web-editor/src/main.tsx
@@ -1,16 +1,24 @@
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from "./app/app";
 
 import * as Sentry from "@sentry/react";
 import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from "react-router-dom";
 
+/**
+ * Initializes Sentry to automatically track errors and performance issues.
+ */
 Sentry.init({
   dsn: "https://4e6f4dd638ec41368bed874cb17dde3a@o574983.ingest.sentry.io/4505489238982656",
   integrations: [
     new Sentry.BrowserTracing({
-      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      /**
+       * Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled.
+       */
       tracePropagationTargets: ["localhost", /^https:\/\/app.openpra\.org/],
+      /**
+       * Configures Sentry's routing instrumentation for React Router v6.
+       */
       routingInstrumentation: Sentry.reactRouterV6Instrumentation(
         React.useEffect,
         useLocation,
@@ -21,16 +29,32 @@ Sentry.init({
     }),
     new Sentry.Replay(),
   ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, could be reduced in production
-  // Session Replay
-  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  /**
+   * Performance Monitoring configuration.
+   * @remarks Capture 100% of the transactions for detailed insights.
+   */
+  tracesSampleRate: 1.0,
+  /**
+   * Session Replay configuration.
+   * @remarks Set the session sample rate for capturing user sessions.
+   */
+  replaysSessionSampleRate: 1.0,
+  /**
+   * Error-based Session Replay configuration.
+   * @remarks Increase the sample rate when errors occur to capture more information.
+   */
+  replaysOnErrorSampleRate: 1.0,
 });
 
+// Select the DOM element where the React application will be attached.
+const container = document.getElementById('root') as HTMLElement;
 
-ReactDOM.render(
+// Create a root for the React application.
+const root = createRoot(container);
+
+// Render the React application within the StrictMode component for highlighting potential problems.
+root.render(
   <StrictMode>
-         <App />
+    <App />
   </StrictMode>
-, document.getElementById('root') as HTMLElement);
+);

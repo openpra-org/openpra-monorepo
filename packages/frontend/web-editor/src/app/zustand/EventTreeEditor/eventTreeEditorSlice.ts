3654e9bf1065c3f3192daf668d02d9788a22a328
aagarw38 <aagarw38@ncsu.edu>
2024-03-30 16:26:04 +0000

    delete column

---
+++ packages/frontend/web-editor/src/app/zustand/EventTreeEditor/eventTreeEditorSlice.ts
@@ -0,0 +1,19 @@
+// import { StateCreator } from "zustand";
+// import { SliceResetFns, storeType } from "../Store";
+// import { deleteNodeType, eventTreeEditorData } from "./eventTreeEditorType";
+// import { eventTreeEditorState } from "./eventTreeEditorState";
+//
+// const eventTreeEditorSlice: StateCreator<storeType, [], [], deleteNodeType> = (
+//   set,
+// ) => {
+//   SliceResetFns.add(() => {
+//     set(eventTreeEditorState);
+//   });
+//   return {
+//     data: eventTreeEditorState.data;
+//     setData: () => ;
+//     getData: () => Promise<void>;
+//   };
+// };
+//
+// export { eventTreeEditorSlice };
---
+++ packages/frontend/web-editor/src/app/zustand/EventTreeEditor/eventTreeEditorType.ts
@@ -0,0 +1,14 @@
+// import { Node } from "reactflow";
+// import { EventTreeData } from "shared-types/src/lib/types/reactflowGraph/graphData/EventTreeData";
+//
+// export type eventTreeEditorType = {
+//   data: eventTreeEditorData;
+//   setData: () => Promise<void>;
+//   getData: () => Promise<void>;
+// };
+//
+// export type eventTreeEditorData = {
+//   isDeleteColumn: string;
+//   allTreeNodes: Node<EventTreeData>[];
+//   tentativeTreeNodes: Node<EventTreeData>[];
+// };

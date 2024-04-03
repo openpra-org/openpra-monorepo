36903db4091c09fe0cdd9c434e250a26baf79e0f
magarwa3 <magarwa3@ncsu.edu>
2024-04-03 13:45:52 +0000

    Added settings Button on breadcrumbs

---
+++ packages/frontend/web-editor/src/app/components/buttons/CreateSettingButton.tsx
@@ -0,0 +1,22 @@
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {
+  EuiButtonEmpty,
+} from "@elastic/eui";
+
+const CreateSettingButton = (): JSX.Element => {
+  const location = useLocation().pathname;
+  const settings_path = location+'/settings';
+
+  return(
+
+    <Link to={settings_path}>
+      <EuiButtonEmpty size="s" color="text" iconType="gear">
+        Settings
+      </EuiButtonEmpty>
+    </Link>
+
+  )
+}
+
+export {CreateSettingButton};

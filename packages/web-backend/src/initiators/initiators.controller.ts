f1c8b216ac73426334850c9d07f981a93f33ca53
nrpancho <nrpancho@ncsu.edu>
2024-04-16 03:07:15 +0000

    CRUD operations for initiators.

--- packages/web-backend/src/initiators/initiators.controller.ts
+++ packages/web-backend/src/initiators/initiators.controller.ts
@@ -18,6 +18,7 @@ import { JwtAuthGuard } from "../guards/jwt-auth.guard";
 import { Public } from "../guards/public.guard";
 import { InvalidTokenFilter } from "../filters/invalid-token.filter";
 import { InitiatorService } from "./initiators.service";
+import { Initiator } from "./schemas/initiator.schema";
   
 @Controller()
 export class InitiatorController {
@@ -25,5 +26,35 @@ constructor(private readonly initiatorService: InitiatorService) {}
     @Get("/hello/")
     async helloWorld(): Promise<string> {
         return "Hello World!";
+    }
+
+    //get all initiators
+    @Get("/initiators/")
+    async getAllInitiators(): Promise<Initiator[]> {
+        return this.initiatorService.getAllInitiators();
+    }
+
+    //get initiator by id
+    @Get("/initiator/:id")
+    async getInitiatorById(@Param("id") id: string): Promise<Initiator | null> {
+        return this.initiatorService.getInitiatorById(id);
+    }
+
+    //create new initiator
+    @Post("/initiator/")
+    async createInitiator(@Body() initiator: Initiator): Promise<Initiator> {
+        return this.initiatorService.createInitiator(initiator);
+    }
+
+    //update initiator by id
+    @Put("/initiator/:id")
+    async updateInitiator(@Param("id") id: string, @Body() initiator: Initiator): Promise<Initiator | null> {
+        return this.initiatorService.updateInitiator(id, initiator);
+    }
+
+    //delete initiator by id
+    @Delete("/initiator/:id")
+    async deleteInitiator(@Param("id") id: string): Promise<Initiator | null> {
+        return this.initiatorService.deleteInitiator(id);
     }
 }
--- packages/web-backend/src/initiators/initiators.service.ts
+++ packages/web-backend/src/initiators/initiators.service.ts
@@ -13,4 +13,31 @@ export class InitiatorService {
     @InjectModel(Initiator.name)
     private readonly initiatorModel: Model<InitiatorDocument>,
   ) {}
+
+  
+  async getAllInitiators(): Promise<Initiator[]> {
+    const initiators = await this.initiatorModel.find()
+    return initiators;
+  };
+
+  async getInitiatorById(id: string): Promise<Initiator | null> {
+    const initiator = await this.initiatorModel.findById(id);
+    return initiator;
+  }
+
+  async createInitiator(initiator: Initiator): Promise<Initiator> {
+    const newInitiator = new this.initiatorModel(initiator);
+    return newInitiator.save();
+  }
+
+  async updateInitiator(id: string, initiator: Initiator): Promise<Initiator | null> {
+    const updatedInitiator = await this.initiatorModel
+      .findByIdAndUpdate(id, initiator, { new: true });
+    return updatedInitiator;
+  }
+
+  async deleteInitiator(id: string): Promise<Initiator | null> {
+    const deletedInitiator = await this.initiatorModel.findByIdAndDelete(id);
+    return deletedInitiator;
+  }
 }

55ec0b3240555d3e4b0b2f2dbc82479b61a5d75b
nrpancho <nrpancho@ncsu.edu>
2024-04-29 19:47:49 +0000

    added heatbalance fault tree to initiator module.

--- packages/web-backend/src/initiators/initiators.module.ts
+++ packages/web-backend/src/initiators/initiators.module.ts
@@ -1,13 +1,19 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import {
+  HeatBalanceFaultTree,
+  HeatBalanceFaultTreeSchema,
+} from "../nestedModels/schemas/heat-balance-fault-tree.schema";
 import { InitiatorService } from "./initiators.service";
 import { InitiatorController } from "./initiators.controller";
 import { Initiator, InitiatorSchema } from "./schemas/initiator.schema";
+//import heatbalance fault tree schema
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-        { name: Initiator.name, schema: InitiatorSchema },
+      { name: Initiator.name, schema: InitiatorSchema },
+      { name: HeatBalanceFaultTree.name, schema: HeatBalanceFaultTreeSchema },
     ]),
   ],
   controllers: [InitiatorController],

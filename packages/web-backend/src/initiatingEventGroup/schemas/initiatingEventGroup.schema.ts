31845fe2a686228724896aab45435813e7bd1233
nrpancho <nrpancho@ncsu.edu>
2024-05-04 13:56:35 +0000

    added timestamps

--- packages/web-backend/src/initiatingEventGroup/schemas/initiatingEventGroup.schema.ts
+++ packages/web-backend/src/initiatingEventGroup/schemas/initiatingEventGroup.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
-@Schema({ versionKey: false })
+@Schema({ versionKey: false, timestamps: true})
 export class InitiatingEventGroup {
   @Prop({ unique: true })
   name: string;

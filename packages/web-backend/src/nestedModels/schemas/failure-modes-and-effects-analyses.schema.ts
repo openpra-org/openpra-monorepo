22d8b545f052214c5fae503d1dbbaad5f6033efa
Nisarg Prajapati <nprajap@ncsu.edu>
2024-05-10 17:20:24 -0400

    Refactoring entire fmea backend endpoint extending nested model

--- packages/web-backend/src/nestedModels/schemas/failure-modes-and-effects-analyses.schema.ts
+++ packages/web-backend/src/nestedModels/schemas/failure-modes-and-effects-analyses.schema.ts
@@ -1,9 +1,45 @@
-import { Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 import { NestedModel } from "./templateSchema/nested-model.schema";
 
+const generateDefaultColumns = () => {
+  const columnNames = [
+    "Subsystem",
+    "Major Components",
+    "Functions",
+    "Failure Modes",
+    "Cause",
+    "Failure Effects",
+    "Preventative Measures",
+    "Postulated Resulting Initiating Events",
+  ];
+
+  return columnNames.map((name) => ({
+    id: crypto.randomUUID(),
+    name: name,
+    type: "string",
+    dropdownOptions: [],
+  }));
+};
 @Schema({ versionKey: false })
-export class FailureModesAndEffectsAnalyses extends NestedModel {}
+export class FailureModesAndEffectsAnalyses extends NestedModel {
+  @Prop({ default: generateDefaultColumns })
+  columns: {
+    id: string;
+    name: string;
+    type: string;
+    dropdownOptions: {
+      number: number;
+      description: string;
+    }[];
+  }[];
+
+  @Prop()
+  rows: {
+    id: number;
+    row_data: Map<string, string>;
+  }[];
+}
 
 export type FailureModesAndEffectsAnalysesDocument =
   FailureModesAndEffectsAnalyses & Document;

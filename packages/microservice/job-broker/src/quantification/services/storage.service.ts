a99bc23d30de5781ccfb3ca52cd5a986c9e27a14
Hasibul H Rasheeq <hrashee@ncsu.edu>
2024-08-11 01:05:17 +0000

    - Minor patch for database related error handling.

--- packages/microservice/job-broker/src/quantification/services/storage.service.ts
+++ packages/microservice/job-broker/src/quantification/services/storage.service.ts
@@ -24,46 +24,49 @@ export class StorageService implements OnApplicationBootstrap {
       return;
     }
 
-    try {
-      // Connect to the RabbitMQ server, create a channel, and connect to
-      // the completed job queue to collect the quantification results
-      const connection = await amqp.connect(url);
-      const channel = await connection.createChannel();
-      await channel.assertQueue(completedQ, { durable: true });
+    // Connect to the RabbitMQ server, create a channel, and connect to
+    // the completed job queue to collect the quantification results
+    const connection = await amqp.connect(url);
+    const channel = await connection.createChannel();
+    await channel.assertQueue(completedQ, { durable: true });
 
-      // Perform some sort of action on the results (e.g., store them in the database etc.)
-      await channel.consume(
-        completedQ,
-        (msg: ConsumeMessage | null) => {
-          if (msg === null) {
-            // Handle the case where no message was available
-            Logger.error("Unable to parse message from quantification storage queue");
-            return;
-          }
+    // Perform some sort of action on the results (e.g., store them in the database etc.)
+    await channel.consume(
+      completedQ,
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      async (msg: ConsumeMessage | null) => {
+        if (msg === null) {
+          // Handle the case where no message was available
+          Logger.error("Unable to parse message from quantification storage queue");
+          return;
+        }
 
+        try {
           // Convert the quantification report in a JSON object and
           // store this JSON object in the database
           const quantifiedReport: QuantifyReport = typia.json.assertParse<QuantifyReport>(msg.content.toString());
           const report = new this.quantifiedReportModel(quantifiedReport);
-          void report.save();
+          await report.save();
           // Finally, acknowledge the message
           channel.ack(msg);
-        },
-        {
-          // Since we are manually acknowledging the message, turn auto acknowledging off
-          noAck: false,
-        },
-      );
-    } catch (error) {
-      if (error instanceof TypeGuardError) {
-        Logger.error(`Validation failed: ${error.path} is invalid. Expected ${error.expected} but got ${error.value}`);
-      } else if (error instanceof mongoose.Error.ValidationError) {
-        for (const field in error.errors) {
-          Logger.error(error.errors[field].message);
+        } catch (error) {
+          if (error instanceof TypeGuardError) {
+            Logger.error(
+              `Validation failed: ${error.path} is invalid. Expected ${error.expected} but got ${error.value}`,
+            );
+          } else if (error instanceof mongoose.Error.ValidationError) {
+            for (const field in error.errors) {
+              Logger.error(error.errors[field].message);
+            }
+          } else {
+            Logger.error("Something went wrong in the quantification storage service.");
+          }
         }
-      } else {
-        Logger.error("Something went wrong in the quantification storage service.");
-      }
-    }
+      },
+      {
+        // Since we are manually acknowledging the message, turn auto acknowledging off
+        noAck: false,
+      },
+    );
   }
 }

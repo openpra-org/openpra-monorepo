76f64c0b76be4603e31a703587eedf6c3cd5e91d
Arjun Earthperson <a.earthperson@openpra.org>
2024-08-21 20:59:05 +0000

    - WIP refactor Ability.ts for auth context support

--- packages/frontend/web-editor/src/app/providers/ability/AbilityProvider.tsx
+++ packages/frontend/web-editor/src/app/providers/ability/AbilityProvider.tsx
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 import { createContextualCan } from "@casl/react";
-import { AppAbility, DefaultAbility } from "../casl/ability";
+import { AppAbility, DefaultAbility } from "./Ability";
 
 const AbilityContext = createContext<AppAbility>(DefaultAbility());
 const Can = createContextualCan(AbilityContext.Consumer);

438331f3edf0e9cdb210a2c11ceac2ff1d675c67
Arjun Earthperson <a.earthperson@openpra.org>
2024-08-23 23:42:54 +0000

    OPENPRA-181 fix-nx-frontend-issues - remove proxy.conf.json - update webpack.config.js - update workspaces in package.json - remove proxy.conf.json - update webpack.config.js - updagte workspaces in package.json
    Merge-request: OPENPRA-MR-143
    Merged-by: Mihai Diaconeasa <madiacon@ncsu.edu>

--- packages/frontend/web-editor/webpack.config.js
+++ packages/frontend/web-editor/webpack.config.js
@@ -3,11 +3,24 @@ const { composePlugins, withNx } = require("@nx/webpack");
 const { withReact } = require("@nx/react");
 
 // Nx plugins for webpack.
+// TODO:: Review https://nx.dev/recipes/webpack/webpack-config-setup#basic-configuration-for-nx
 module.exports = composePlugins(
   withNx(),
   withReact(),
-  (config) =>
-    // Update the webpack config as needed here.
-    // e.g. `config.plugins.push(new MyPlugin())`
-    config,
+  (config) => {
+    config.devServer = {
+      port: 4200,
+      proxy: [
+        {
+          context: ["/api"],
+          target: "http://localhost:8000",
+          secure: true,
+          changeOrigin: true,
+        },
+      ],
+    };
+    return config;
+  },
+  // Update the webpack config as needed here.
+  // e.g. `config.plugins.push(new MyPlugin())`
 );

eb58a29eaea4b7ba14518d9b0a8ff4b71435b706
Arjun Earthperson <a.earthperson@openpra.org>
2024-08-23 23:14:57 +0000

    - initial fix

--- packages/frontend/web-editor/src/app/casl/ability.ts
+++ packages/frontend/web-editor/src/app/casl/ability.ts
@@ -1,19 +1,19 @@
 import { AbilityBuilder, createMongoAbility, MongoAbility, RawRuleOf } from "@casl/ability";
-import { Role } from "shared-types/src/openpra-mef/role/role-type";
 import { GetAllRoles } from "shared-types/src/lib/api/roles/rolesApi";
+import { PermissionSchemaDto, RoleSchemaDto } from "shared-types/src/openpra-zod-mef/role/role-schema";
 
 type Actions = "create" | "read" | "update" | "delete" | "manage";
 type Subjects = "users" | "roles" | "invitation" | "all";
 type AppAbility = MongoAbility<[Actions, Subjects]>;
 
-const DefineAbility = (roles: Role[]): MongoAbility<[Actions, Subjects]> => {
+const DefineAbility = (roles: PermissionSchemaDto): MongoAbility<[Actions, Subjects]> => {
   const rules = roles as unknown as RawRuleOf<AppAbility>[];
   return createMongoAbility<AppAbility>(rules);
 };
 
 async function UpdateAbility(ability: AppAbility, roles: string[]): Promise<void> {
-  const fetchedRoles = await GetAllRoles(roles);
-  const extractedRoles = fetchedRoles.map((fetchedRole) => {
+  const fetchedRoles: RoleSchemaDto[] = await GetAllRoles(roles);
+  const extractedRoles: PermissionSchemaDto[] = fetchedRoles.map((fetchedRole: RoleSchemaDto) => {
     return fetchedRole.permissions;
   });
   const { rules } = DefineAbility(extractedRoles.flat());
--- packages/shared-types/src/openpra-zod-mef/role/role-schema.ts
+++ packages/shared-types/src/openpra-zod-mef/role/role-schema.ts
@@ -5,35 +5,31 @@ To change the Zod Dto update the relevant schema file
 import { z } from "nestjs-zod/z";
 import { createZodDto } from "nestjs-zod";
 
+const PermissionSchema = z
+  .array(
+    z.object({
+      action: z
+        .union([z.string().min(5).max(255), z.array(z.string().min(5).max(255))])
+        .describe("The actions that a role can perform"),
+      subject: z
+        .union([z.string().min(5).max(255), z.array(z.string().min(5).max(255))])
+        .describe("The actions that a role can perform"),
+      fields: z.any().optional(),
+      conditionals: z.any().optional(),
+      inverted: z.boolean().optional(),
+      reason: z.string().min(1).max(255).optional(),
+    }),
+  )
+  .describe("This will define the core permissions");
+
 const RoleSchema = z
   .object({
     id: z.string().min(5).max(255).describe("The unique id for role"),
     name: z.string().min(5).max(255).describe("The name of the role"),
-    permissions: z
-      .array(
-        z.object({
-          action: z
-            .union([
-              z.string().min(5).max(255),
-              z.array(z.string().min(5).max(255)),
-            ])
-            .describe("The actions that a role can perform"),
-          subject: z
-            .union([
-              z.string().min(5).max(255),
-              z.array(z.string().min(5).max(255)),
-            ])
-            .describe("The actions that a role can perform"),
-          fields: z.any().optional(),
-          conditionals: z.any().optional(),
-          inverted: z.boolean().optional(),
-          reason: z.string().min(1).max(255).optional(),
-        }),
-      )
-      .describe("This will define the core permissions"),
+    permissions: PermissionSchema,
   })
   .describe("A role in the backend");
 
-class RoleSchemaDto extends createZodDto(RoleSchema) {}
+export class PermissionSchemaDto extends createZodDto(PermissionSchema) {}
 
-export { RoleSchemaDto };
+export class RoleSchemaDto extends createZodDto(RoleSchema) {}

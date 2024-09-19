92040174bba72198097724de4ea0bdd0569b4396
Arjun Earthperson <a.earthperson@openpra.org>
2024-09-19 16:05:50 -0400

    - Add comments for ValidatedFieldText.tsx - Add debounce checking logic

--- packages/frontend/web-editor/src/app/auth/signup/ValidatedFieldText.tsx
+++ packages/frontend/web-editor/src/app/auth/signup/ValidatedFieldText.tsx
@@ -7,25 +7,54 @@ import { ValidationResult } from "shared-types/src/lib/validation/ValidationResu
 import { EuiFormRowProps } from "@elastic/eui/src/components/form/form_row/form_row";
 import { useDebouncedCallback } from "use-debounce";
 
+/**
+ * Interface representing the result of a validated field.
+ *
+ * @typeparam DataType - The type of the value being validated.
+ */
 export interface ValidatedFieldResult<DataType> {
+  /** Indicates if the field is invalid. */
   isInvalid: boolean;
+  /** Array of error messages, if any. */
   errors?: string[];
+  /** The value of the field. */
   value: DataType;
 }
 
+/**
+ * Props for the `ValidatedFieldText` component.
+ */
 export type ValidatedFieldTextProps = EuiFieldTextProps & {
+  /** Array of regex validation rules to apply. */
   regexRules?: NonEmptyArray<RegexValidationRule>;
+  /** If true, the field will not be wrapped in a form row. */
   noFormRow?: boolean;
+  /** Additional props for the form row. */
   formRowProps?: Omit<EuiFormRowProps, "children">;
+  /** Callback function to handle changes in the field value. */
   onChanged?: (changeResult: ValidatedFieldResult<string>) => void;
-  noDebounce?: boolean; // TODO:: implement debounce if unset
+  /** If true, debounce will not be applied to the field. */
+  noDebounce?: boolean;
+  /** default debounce timeout of 1000ms */
+  debounceTimeoutMs?: number;
 };
 
+/**
+ * Props for the `FormRowWrapped` component.
+ */
 type FormRowWrappedProps = EuiFormRowProps & {
+  /** If true, the field will not be wrapped in a form row. */
   noFormRow?: boolean;
+  /** The child element to be wrapped. */
   children: ReactElement;
 };
 
+/**
+ * A component that conditionally wraps its children in an `EuiFormRow`.
+ *
+ * @param props - The props for the component.
+ * @returns The wrapped or unwrapped child element.
+ */
 const FormRowWrapped = (props: FormRowWrappedProps): ReactElement => {
   const { noFormRow, children, ...others } = props;
 
@@ -36,16 +65,33 @@ const FormRowWrapped = (props: FormRowWrappedProps): ReactElement => {
   return <EuiFormRow {...others}>{children}</EuiFormRow>;
 };
 
+/**
+ * A validated text field component that applies regex validation rules.
+ *
+ * @param props - The props for the component.
+ * @returns The validated text field component.
+ *
+ * @example
+ * ```
+ * <ValidatedFieldText
+ *   regexRules={[{ pattern: /^[a-z]+$/, message: "Only lowercase letters allowed" }]}
+ *   onChanged={(result) => console.log(result)}
+ * />
+ * ```
+ */
 export const ValidatedFieldText = (props: ValidatedFieldTextProps): ReactElement => {
-  const { regexRules, noFormRow, formRowProps, onChanged, ...others } = props;
+  const { regexRules, noFormRow, formRowProps, noDebounce, debounceTimeoutMs, onChanged, ...others } = props;
 
   const [pristine, setPristine] = useState<boolean>(true);
   const [value, setValue] = useState<string>("");
 
-  const debouncedChange = useDebouncedCallback(() => {
-    setPristine(false);
-    handleChange();
-  }, 1000);
+  const debouncedChange = useDebouncedCallback(
+    () => {
+      setPristine(false);
+      handleChange();
+    },
+    debounceTimeoutMs ? debounceTimeoutMs : 1000,
+  );
 
   const handleChange = (): void => {
     const changeResult: ValidatedFieldResult<string> = {
@@ -70,7 +116,7 @@ export const ValidatedFieldText = (props: ValidatedFieldTextProps): ReactElement
         isInvalid={isInvalid}
         onChange={(e) => {
           setValue(String(e.target.value)); // set the value, let the component re-render/re-apply validations, etc.
-          debouncedChange();
+          noDebounce ? handleChange() : debouncedChange();
         }}
         onBlur={() => {
           handleChange();

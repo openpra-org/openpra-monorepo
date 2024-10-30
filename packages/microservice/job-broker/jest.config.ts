dc3ecfc765683f5a9486fe505c1745f04dce1020
Hasibul H Rasheeq <hrashee@ncsu.edu>
2024-10-30 16:36:11 -0400

    OPENPRA-169 - Removed the default test settings related to mongo-memory-server.

--- packages/microservice/job-broker/jest.config.ts
+++ packages/microservice/job-broker/jest.config.ts
@@ -7,10 +7,5 @@ export default {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
   moduleFileExtensions: ["ts", "js", "html"],
-  snapshotFormat: { escapeString: true, printBasicPrototype: true },
-  globalSetup: "<rootDir>/tests/unitTestSetup.ts",
-  globalTeardown: "<rootDir>/tests/unitTestTeardown.ts",
-  maxWorkers: 1,
-  collectCoverage: true,
   coverageDirectory: "../../../coverage/packages/microservice-job-broker",
 };

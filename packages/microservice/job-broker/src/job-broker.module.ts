93db6513f79dfe40f44968c29312724fb20e19fe
Arjun Earthperson <a.earthperson@openpra.org>
2025-01-10 19:57:31 +0000

    - `job-broker` module should use `MONGO_URL` env var

--- packages/microservice/job-broker/src/job-broker.module.ts
+++ packages/microservice/job-broker/src/job-broker.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { APP_FILTER, RouterModule } from "@nestjs/core";
 import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { EnvVarKeys } from "../config/env_vars.config";
 import { HttpExceptionFilter } from "./http-exception.filter";
 import { JobBrokerController } from "./job-broker.controller";
@@ -17,7 +18,13 @@ import { ExecutableJobReport, ExecutableJobSchema } from "./middleware/schemas/e
     QuantificationModule,
     ValidationModule,
     ExecutableModule,
-    MongooseModule.forRoot(EnvVarKeys.MONGODB_URI),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>("MONGO_URL"),
+      }),
+    }),
     MongooseModule.forFeature([
       { name: QuantificationJobReport.name, schema: QuantificationJobSchema },
       { name: ExecutableJobReport.name, schema: ExecutableJobSchema },

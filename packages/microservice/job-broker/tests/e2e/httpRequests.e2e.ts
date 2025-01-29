a44f27113ee27580fc840286f4c6a67ae077c34c
Hasibul H Rasheeq <hrashee@ncsu.edu>
2025-01-29 13:03:38 -0500

    - WIP: - Removed Max Length Limit from RabbitMQ.

--- packages/microservice/job-broker/tests/e2e/httpRequests.e2e.ts
+++ packages/microservice/job-broker/tests/e2e/httpRequests.e2e.ts
@@ -8,14 +8,16 @@ const xmlContent = fs.readFileSync("./fixtures/models/generic-openpsa-models/mod
 const model = Buffer.from(xmlContent, "utf-8").toString("base64");
 
 describe("It should send HTTP requests to specified endpoints", () => {
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 10000; i++) {
     it("Should send requests", () => {
       (async () => {
         try {
-          await superagent.post("http://localhost:3000/q/quantify/scram").send({
+          await superagent.post("https://review-cherry-pick-tests-openpra-169.app.openpra.org/q/quantify/scram").send({
             mocus: true,
             mcub: true,
             probability: true,
+            uncertainty: true,
+            "num-trials": 10000,
             models: [model],
           });
         } catch (err) {

d89ae4e3f8efcc2b7b85b0e834b7ad0f89d16773
Hasibul H Rasheeq <hrashee@ncsu.edu>
2025-01-29 13:05:44 -0500

    - WIP: - Fixed two small ESLint problems in the test file.

--- packages/microservice/job-broker/tests/e2e/httpRequests.e2e.ts
+++ packages/microservice/job-broker/tests/e2e/httpRequests.e2e.ts
@@ -10,7 +10,7 @@ const model = Buffer.from(xmlContent, "utf-8").toString("base64");
 describe("It should send HTTP requests to specified endpoints", () => {
   for (let i = 0; i < 10000; i++) {
     it("Should send requests", () => {
-      (async () => {
+      void (async (): Promise<void> => {
         try {
           await superagent.post("https://review-cherry-pick-tests-openpra-169.app.openpra.org/q/quantify/scram").send({
             mocus: true,

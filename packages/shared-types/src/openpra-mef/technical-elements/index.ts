d0320cdd85054292981894e60bd1b4a8872affa1
Rahul Agarwal <111393043+rahulagarwal13@users.noreply.github.com>
2025-01-31 18:11:15 -0800

    typedoc final

--- packages/shared-types/src/openpra-mef/technical-elements/index.ts
+++ packages/shared-types/src/openpra-mef/technical-elements/index.ts
@@ -1,17 +1,35 @@
-/** @module data_analysis */
-export * as data_analysis from './data-analysis/data-analysis';
-
-/** @module plant_operating_state */
+/**
+ * @module plant_operating_state
+ * @description Plant Operating State Analysis types and interfaces
+ */
 export * as plant_operating_state from './plant-operating-state-analysis/plant-operating-state-analysis';
 
-/** @module initiating_event */
+/**
+ * @module data_analysis
+ * @description Data Analysis types and interfaces
+ */
+export * as data_analysis from './data-analysis/data-analysis';
+
+/**
+ * @module initiating_event
+ * @description Initiating Event Analysis types and interfaces
+ */
 export * as initiating_event from './initiating-event-analysis/initiating-event-analysis';
 
-/** @module systems_analysis */
+/**
+ * @module systems_analysis
+ * @description Systems Analysis types and interfaces
+ */
 export * as systems_analysis from './systems-analysis/systems-analysis';
 
-/** @module meta */
+/**
+ * @module meta
+ * @description Core metadata types and interfaces
+ */
 export * as meta from './meta';
 
-/** @module events */
+/**
+ * @module events
+ * @description Event types and hierarchies
+ */
 export * as events from './events'; 

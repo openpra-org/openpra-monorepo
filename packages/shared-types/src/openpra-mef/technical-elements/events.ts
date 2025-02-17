bd6ebe1f03599225053862756d2b6fb488103136
Rahul Agarwal <111393043+rahulagarwal13@users.noreply.github.com>
2025-02-17 13:36:18 -0800

    Data analysis Schema first draft (#7)
    Data analysis schema and technical elements
    
    - Add tech elements and codes
    - add events.ts
    - data analysis schema
    - schema viewer
    - typedoc

---
+++ packages/shared-types/src/openpra-mef/technical-elements/events.ts
@@ -0,0 +1,95 @@
+import typia from "typia";
+import { Named, Unique } from "./meta";
+
+/**
+ * @namespace BaseEvents
+ * @description Base event types and interfaces
+ */
+
+/**
+ * Base Event - parent of all events
+ * @memberof BaseEvents
+ * @extends {Unique}
+ * @extends {Named}
+ */
+export interface BaseEvent extends Unique, Named {
+    description?: string;
+}
+
+/**
+ * @namespace BasicEvents
+ * @description Basic event types and their specializations
+ */
+
+/**
+ * Basic Event type
+ * @memberof BasicEvents
+ * @extends {BaseEvent}
+ */
+export interface BasicEvent extends BaseEvent {
+    eventType: "BASIC";
+}
+
+/**
+ * Licensing Basis Event
+ * @memberof BasicEvents
+ * @extends {BasicEvent}
+ */
+export interface LicensingBasisEvent extends BasicEvent {
+    eventSubType: "LICENSING_BASIS";
+}
+
+/**
+ * @namespace FunctionalEvents
+ * @description Functional event types and their specializations
+ */
+
+/**
+ * Functional Event type
+ * @memberof FunctionalEvents
+ * @extends {BaseEvent}
+ */
+export interface FunctionalEvent extends BaseEvent {
+    eventType: "FUNCTIONAL";
+}
+
+/**
+ * Top Event
+ * @memberof FunctionalEvents
+ * @extends {FunctionalEvent}
+ */
+export interface TopEvent extends FunctionalEvent {
+    eventSubType: "TOP";
+}
+
+/**
+ * @namespace InitiatingEvents
+ * @description Initiating event types
+ */
+
+/**
+ * Initiating Event type
+ * @memberof InitiatingEvents
+ * @extends {BaseEvent}
+ */
+export interface InitiatingEvent extends BaseEvent {
+    eventType: "INITIATING";
+}
+
+/**
+ * @namespace Validation
+ * @description Schema validation for events
+ */
+
+/**
+ * Event validation schemas
+ * @memberof Validation
+ */
+export const EventSchemas = {
+    base: typia.json.application<[BaseEvent], "3.0">(),
+    basic: typia.json.application<[BasicEvent], "3.0">(),
+    licensing: typia.json.application<[LicensingBasisEvent], "3.0">(),
+    functional: typia.json.application<[FunctionalEvent], "3.0">(),
+    top: typia.json.application<[TopEvent], "3.0">(),
+    initiating: typia.json.application<[InitiatingEvent], "3.0">()
+} as const;
---
+++ packages/shared-types/src/openpra-mef/technical-elements/index.ts
@@ -0,0 +1,35 @@
+/**
+ * @module plant_operating_state
+ * @description Plant Operating State Analysis types and interfaces
+ */
+export * as plant_operating_state from './plant-operating-state-analysis/plant-operating-state-analysis';
+
+/**
+ * @module data_analysis
+ * @description Data Analysis types and interfaces
+ */
+export * as data_analysis from './data-analysis/data-analysis';
+
+/**
+ * @module initiating_event
+ * @description Initiating Event Analysis types and interfaces
+ */
+export * as initiating_event from './initiating-event-analysis/initiating-event-analysis';
+
+/**
+ * @module systems_analysis
+ * @description Systems Analysis types and interfaces
+ */
+export * as systems_analysis from './systems-analysis/systems-analysis';
+
+/**
+ * @module meta
+ * @description Core metadata types and interfaces
+ */
+export * as meta from './meta';
+
+/**
+ * @module events
+ * @description Event types and hierarchies
+ */
+export * as events from './events'; 

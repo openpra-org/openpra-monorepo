679117fc70fd63b733bc9ed05e1cddb504c965bb
Aditi Vakeel <avakeel@ncsu.edu>
2025-02-21 12:11:41 -0500

    OPENPRA-10 EventTreeEditor   Outcome:   - Change creating new categories from prompt to generic modal

--- packages/frontend/web-editor/src/app/components/treeNodes/eventTreeEditorNode/outputNode.tsx
+++ packages/frontend/web-editor/src/app/components/treeNodes/eventTreeEditorNode/outputNode.tsx
@@ -1,7 +1,9 @@
 import { Handle, NodeProps, Position } from "reactflow";
 import React, { useState, useEffect } from "react";
-import { EuiText, EuiSelect } from "@elastic/eui";
+import { EuiText, EuiSelect, EuiSelectOption } from "@elastic/eui";
 import { useCategoryContext } from "../../../hooks/eventTree/useCreateReleaseCategory";
+import { GenericModal } from "../../modals/genericModal";
+import { CategoryForm } from "./categoryForm";
 import styles from "./styles/nodeTypes.module.css";
 
 // Counter for sequence IDs
@@ -29,6 +31,9 @@ function OutputNode({ id, data }: NodeProps) {
   const { categories, addCategory } = useCategoryContext();
   const [releaseCategory, setReleaseCategory] = useState(data.label);
   const [sequenceId, setSequenceId] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [newCategory, setNewCategory] = useState("");
+  const selectOptions = [...categories, { value: "Create new", text: "Create new .." }];
 
   // Initialize sequence ID on mount
   useEffect(() => {
@@ -49,14 +54,25 @@ function OutputNode({ id, data }: NodeProps) {
   const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const value = e.target.value;
     if (value === "Create new") {
-      const newCategory = prompt("Enter new Release Category:");
-      if (newCategory) {
-        addCategory(newCategory); // Add new category globally
-        setReleaseCategory(newCategory); // Set selected category
-      }
+      // const newCategory = prompt("Enter new Release Category:");
+      setIsModalVisible(true);
     } else {
       setReleaseCategory(value);
     }
+  };
+
+  const handleModalSubmit = async (): Promise<void> => {
+    if (newCategory.trim()) {
+      addCategory(newCategory);
+      setReleaseCategory(newCategory);
+      setNewCategory("");
+      setIsModalVisible(false);
+    }
+  };
+
+  const handleModalClose = () => {
+    setNewCategory("");
+    setIsModalVisible(false);
   };
 
   // Format the display label
@@ -96,19 +112,37 @@ function OutputNode({ id, data }: NodeProps) {
       >
         {/* Show dropdown for Release Category */}
         {data.label === "Category A" || data.label === "Category B" ? (
-          <EuiSelect
-            options={[...categories, ...staticOptions]}
-            value={releaseCategory}
-            onChange={handleCategoryChange}
-            compressed={true} // Makes dropdown smaller
-            style={{
-              width: "115px", // Smaller dropdown width
-              height: "30px", // Smaller dropdown height
-              fontSize: "0.7rem", // Smaller font size
-              padding: "0 5px",
-              boxSizing: "border-box",
-            }}
-          />
+          <>
+            <EuiSelect
+              // options={[...categories, ...staticOptions]}
+              options={selectOptions}
+              value={releaseCategory}
+              onChange={handleCategoryChange}
+              compressed={true} // Makes dropdown smaller
+              style={{
+                width: "115px", // Smaller dropdown width
+                height: "30px", // Smaller dropdown height
+                fontSize: "0.7rem", // Smaller font size
+                padding: "0 5px",
+                boxSizing: "border-box",
+              }}
+            />
+            {isModalVisible && (
+              <GenericModal
+                title="Add New Release Category"
+                body={
+                  <CategoryForm
+                    newCategory={newCategory}
+                    setNewCategory={setNewCategory}
+                  />
+                }
+                onClose={handleModalClose}
+                onSubmit={handleModalSubmit}
+                modalFormId="add-category-form"
+                showButtons={true}
+              />
+            )}
+          </>
         ) : (
           <EuiText style={{ fontSize: "0.7rem" }}>{getDisplayLabel()}</EuiText>
         )}

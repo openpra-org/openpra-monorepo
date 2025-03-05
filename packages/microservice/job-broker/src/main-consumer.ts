45c7b77c7c7fc48096e436ffa1e361efa2056578
Hasibul H Rasheeq <hrashee@ncsu.edu>
2025-03-05 17:47:52 -0500

    - Fixed the `Consumer not connected to the queue` issue. - The port number provided in app.listen() method is a placebo port number. We do not have to actually listen to the port, but since app.listen() method requires a port number to initialize the worker services, we are providing a random port number.

--- packages/microservice/job-broker/src/main-consumer.ts
+++ packages/microservice/job-broker/src/main-consumer.ts
@@ -1,6 +1,6 @@
-import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
-import { Transport } from "@nestjs/microservices";
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
+import { ConfigService } from "@nestjs/config";
 import { EnvVarKeys } from "../config/env_vars.config";
 import { QuantificationConsumerModule } from "./quantification/quantification-consumer.module";
 
@@ -8,13 +8,17 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(QuantificationConsumerModule);
   const configService: ConfigService = app.get(ConfigService);
   const url = configService.getOrThrow<string>(EnvVarKeys.ENV_RABBITMQ_URL);
-  const connected = app.connectMicroservice({
+  app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
       urls: [url],
     },
   });
-  await connected.listen();
+  await app.startAllMicroservices();
+  // The port number provided in the app.listen() method does not have to be exposed in Docker.
+  // This is a placebo port number. We do not have to actually listen to the port, but we need the app.listen() method
+  // to properly launch the app. And the app.listen() method requires a port number.
+  await app.listen(5555);
 }
 
 void bootstrap();

81c86f613f48d7bfe47ac452856218493284f5f8
Hasibul H Rasheeq <hrashee@ncsu.edu>
2025-03-05 17:47:52 -0500

    - Fixed the `Consumer not connected to the queue` issue. - The port number provided in app.listen() method is a placebo port number. We do not have to actually listen to the port, but since app.listen() method requires a port number to initialize the worker services, we are providing a random port number.

--- packages/microservice/job-broker/src/main-consumer.ts
+++ packages/microservice/job-broker/src/main-consumer.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 import { ConfigService } from "@nestjs/config";

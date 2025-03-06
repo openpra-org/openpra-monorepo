e669c5e5563be5cd9363b203fd3e8bac2024a552
Hasibul H Rasheeq <hrashee@ncsu.edu>
2025-03-05 22:55:59 -0500

    - Removed logger utility from ExecutableStorage Service.

--- packages/microservice/job-broker/src/executable/services/executable-storage.service.ts
+++ packages/microservice/job-broker/src/executable/services/executable-storage.service.ts
@@ -1,12 +1,10 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { ExecutableJobReport } from "../../middleware/schemas/executable-job.schema";
 
 @Injectable()
 export class ExecutableStorageService {
-  private readonly logger = new Logger(ExecutableStorageService.name);
-
   constructor(@InjectModel(ExecutableJobReport.name) private readonly executableJobModel: Model<ExecutableJobReport>) {}
 
   /**

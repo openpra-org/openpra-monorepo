8b746dfec1e8366967b56ceb2f4a0537b0576f0d
Hasibul H Rasheeq <hrashee@ncsu.edu>
2025-03-12 23:11:30 -0400

    - Created generic worker module for Quantification and Executable services. Any newly created service can now just be added to this module. This enables us to launch a worker app that can perform multiple services.

--- packages/microservice/job-broker/src/main-consumer.ts
+++ packages/microservice/job-broker/src/main-consumer.ts
@@ -1,18 +1,11 @@
-import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 import { ConfigService } from "@nestjs/config";
-import { Logger } from "@nestjs/common";
 import { EnvVarKeys } from "../config/env_vars.config";
 import { JobBrokerConsumerModule } from "./job-broker-consumer.module";
 
 async function bootstrap(): Promise<void> {
-  const logger = new Logger();
-
-  logger.debug("Initializing the app...");
   const app = await NestFactory.create(JobBrokerConsumerModule);
-
-  logger.debug("Configuring consumer microservice...");
   const configService: ConfigService = app.get(ConfigService);
   const url = configService.getOrThrow<string>(EnvVarKeys.ENV_RABBITMQ_URL);
   app.connectMicroservice<MicroserviceOptions>({
@@ -21,8 +14,6 @@ async function bootstrap(): Promise<void> {
       urls: [url],
     },
   });
-
-  logger.debug("Starting consumer microservice...");
   await app.startAllMicroservices();
   // The port number provided in the app.listen() method does not have to be exposed in Docker.
   // This is a placebo port number. We do not have to actually listen to the port, but we need the app.listen() method

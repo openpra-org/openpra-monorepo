cd12f8cd63bc0d8801d9a500b1a9d75af50cea1d
Aditi Vakeel <avakeel@ncsu.edu>
2025-03-20 17:24:30 -0400

    OPENPRA-10 EventTreeEditor - create a button to compute frequency(dynamically adjusts position relative to other columns)

--- packages/frontend/web-editor/src/app/components/treeNodes/eventTreeEditorNode/eventTreeNodeType.ts
+++ packages/frontend/web-editor/src/app/components/treeNodes/eventTreeEditorNode/eventTreeNodeType.ts
@@ -3,12 +3,14 @@ import VisibleNode from "./visibleNode";
 import ColumnNode from "./columnNode";
 import OutputNode from "./outputNode";
 import InvisibleNode from "./invisibleNode";
+import ComputeButtonColumn from "./ComputeButtonColumn";
 
 export const nodeTypes: NodeTypes = {
   visibleNode: VisibleNode,
   invisibleNode: InvisibleNode,
   outputNode: OutputNode,
   columnNode: ColumnNode,
+  computeButtonColumn: ComputeButtonColumn,
 };
 
 export default nodeTypes;

c5c1dbd9d5dcdbf48744e09f6707c52b46884956
Hasibul H Rasheeq <hrashee@ncsu.edu>
2025-04-04 14:07:00 -0400

    - Minor linting fixes.

--- packages/microservice/job-broker/src/executable/services/executable.service.ts
+++ packages/microservice/job-broker/src/executable/services/executable.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, InternalServerErrorException, OnApplicationBootstrap } from "@nestjs/common";
+import { Injectable, Logger, OnApplicationBootstrap } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Channel } from "amqplib";

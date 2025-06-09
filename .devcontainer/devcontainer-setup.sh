eac763e98869b5bec5abf513455bd5b097ccc737
rasheeqqua <rasheeq1018182@gmail.com>
2025-06-09 17:28:33 -0400

    Added shell script for MongoDB and RabbitMQ.

--- .devcontainer/devcontainer-setup.sh
+++ .devcontainer/devcontainer-setup.sh
@@ -3,7 +3,7 @@ set -e
 
 # Start MongoDB (in background)
 sudo mkdir -p /data/db
-sudo chown -R vscode:vscode /data/db
+sudo chown -R $(whoami):$(whoami) /data/db
 mongod --fork --logpath /tmp/mongo.log --dbpath /data/db
 
 # Start RabbitMQ (in background)
@@ -12,5 +12,4 @@ sudo service rabbitmq-server start
 # (Optional) Any other setup, e.g., git submodules
 git submodule update --init --recursive
 
-# Print status
 echo "MongoDB and RabbitMQ started."

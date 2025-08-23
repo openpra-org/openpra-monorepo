c65c017cda567691b3d1acf386656fd832dec1e3
Utsavkumar Lal <36764273+utsavll0@users.noreply.github.com>
2025-08-23 16:54:18 +0000

    adding initial logging setup

--- packages/web-backend/src/api.service.ts
+++ packages/web-backend/src/api.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 
 @Injectable()
 export class ApiService {
+  private readonly logger = new Logger(ApiService.name);
+
   getHello(): string {
+    this.logger.log("This is a test logging message!!!"); // Log a simple message
     return "Hello World!";
   }
 }
--- packages/web-backend/src/main.ts
+++ packages/web-backend/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { CorsConfig } from "./cors";
 import { ApiModule } from "./api.module";
+import { LoggerFactory } from "./factory/logger.factory";
 
-async function bootstrap() {
-  const app = await NestFactory.create(ApiModule);
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(ApiModule, {
+    logger: LoggerFactory("web-backend"),
+  });
   app.enableCors(CorsConfig);
   await app.listen(8000);
 }
 
-bootstrap();
+void bootstrap();

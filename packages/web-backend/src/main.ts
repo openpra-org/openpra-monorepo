13abebbaa79f80ab2f99ca9481767aed1230f847
Hasibul Rasheeq <hrashee@ncsu.edu>
2025-09-15 02:54:20 +0000

    Added express package to increase request body limit on web-backend.

--- packages/web-backend/src/main.ts
+++ packages/web-backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { CorsConfig } from "./cors";
+import { json, urlencoded } from "express";
 import { ApiModule } from "./api.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
   app.enableCors(CorsConfig);
+  app.use(json({ limit: "50mb" }));
+  app.use(urlencoded({ extended: true, limit: "50mb" }));
   await app.listen(8000);
 }
 

dcc9d187e50c2b267aeb8d87a0f90f8e1e94a233
Hasibul Rasheeq <hrashee@ncsu.edu>
2025-09-27 14:47:43 +0000

    Refactor: Started to bring `scram-node` endpoint to `scram` endpoint.

--- packages/microservice/job-broker/config/env_vars.config.ts
+++ packages/microservice/job-broker/config/env_vars.config.ts
@@ -1,10 +1,11 @@
 import { RabbitMQConfigKeys, RabbitMQEnvVars } from "./rabbitmq.config";
 import { JwtEnvVarKeys, JwtEnvVars } from "./jwt.config";
+import { MinioEnvVars, MinioConfigKeys } from "./minio.config";
 
 /**
  * Type for environment variable keys combining MongoDB and RabbitMQ keys.
  */
-export type EnvVarKeys = RabbitMQConfigKeys | JwtEnvVarKeys;
+export type EnvVarKeys = RabbitMQConfigKeys | JwtEnvVarKeys | MinioConfigKeys;
 
 /**
  * Object containing all environment variable keys from MongoDB and RabbitMQ configurations.
@@ -12,9 +13,10 @@ export type EnvVarKeys = RabbitMQConfigKeys | JwtEnvVarKeys;
 export const EnvVarKeys = {
   ...RabbitMQConfigKeys,
   ...JwtEnvVarKeys,
+  ...MinioConfigKeys
 };
 
 /**
  * Interface combining MongoDB and RabbitMQ environment variables.
  */
-export interface EnvVars extends RabbitMQEnvVars, JwtEnvVars {}
+export interface EnvVars extends RabbitMQEnvVars, JwtEnvVars, MinioEnvVars {}

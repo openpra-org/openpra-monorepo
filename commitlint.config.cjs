9924fe10ccd1330d569eadca530ed454fb983599
Mihai A. Diaconeasa <madiacon@ncsu.edu>
2025-10-18 17:09:34 +0000

    chore: relax commitlint body-max-line-length to 500

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Allow longer commit body lines for detailed change logs
+    'body-max-line-length': [2, 'always', 500],
+  },
 };

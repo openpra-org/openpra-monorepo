cea8d15bb96180f71557cbbe1851417dd4e2d2c8
Mihai A. Diaconeasa <madiacon@ncsu.edu>
2025-10-22 01:45:22 +0000

    fix(job-broker): use type-only imports from mef-types

--- packages/microservice/job-broker/src/quantification/controllers/ftrex.controller.ts
+++ packages/microservice/job-broker/src/quantification/controllers/ftrex.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, InternalServerErrorException, NotFoundException } from "@nestjs/common";
 import { TypedRoute, TypedBody } from "@nestia/core";
-import { QuantifyRequest } from "mef-types/openpra-mef/util/quantify-request";
+import type { QuantifyRequest } from "mef-types/openpra-mef/util/quantify-request";
 import { ProducerService } from "../services/producer.service";
 import { QuantificationJobReport } from "../../middleware/schemas/quantification-job.schema";
 import { StorageService } from "../services/storage.service";

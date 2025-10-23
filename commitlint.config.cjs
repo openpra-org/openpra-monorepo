f3cbd70f60b3044e596ff7ce0115e4ca1b317879
Mihai A. Diaconeasa <madiacon@ncsu.edu>
2025-10-23 20:35:30 +0000

    chore(commitlint): disable header max length rule

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,5 +3,7 @@ module.exports = {
   rules: {
     // Allow longer commit body lines for detailed change logs
     'body-max-line-length': [2, 'always', 500],
+    // Disable header length restriction
+    'header-max-length': [0],
   },
 };

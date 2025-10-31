6851928033f2c323e48f2af0995f5d11cef6e0cf
Mihai A. Diaconeasa <madiacon@ncsu.edu>
2025-10-31 17:09:03 +0000

    docs(typedoc): add @packageDocumentation and JSDoc to mef-types, model-generator, and web-editor utils; regen coverage

--- packages/mef-types/src/index.ts
+++ packages/mef-types/src/index.ts
@@ -1,1 +1,8 @@
+/**
+ * @packageDocumentation
+ * MEF technical element type utilities and helpers.
+ *
+ * This package contains small runtime helpers and type-centric utilities
+ * around MEF technical elements extracted from shared-types.
+ */
 export * from "./lib/mef-types";
--- packages/model-generator/src/index.ts
+++ packages/model-generator/src/index.ts
@@ -1,1 +1,8 @@
+/**
+ * @packageDocumentation
+ * Public entrypoint for the model-generator utilities.
+ *
+ * Exports helpers for building or transforming models used across the
+ * OpenPRA toolchain. APIs here are considered public and stable.
+ */
 export * from "./lib/model-generator";
